321141ddc44205167707ed02343790752d55287f
vedri45 <vedri45@gmail.com>
2024-05-09 22:02:53 +0700

    Add Auth0 for register/log in

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+require('dotenv').config();
+const http = require('http');
+
+const app = express()
+const { auth } = require('express-openid-connect');
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: 'http://localhost:3000',
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.AUTH0_DOMAIN
+};
+
+const port = process.env.PORT || 3000;
+if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
+    config.baseURL = `http://localhost:${port}`;
+}
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+http.createServer(app)
+    .listen(port, () => {
+        console.log(`Listening on ${config.baseURL}`);
+    });
